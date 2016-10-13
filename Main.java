bf6623ed9f23642f921893ce37a6d4b52a7a1bed
Pegah-Eizadkhah <peizadkh@gmail.com>
2016-10-12 22:06:31 -0400

    Edited description

--- Main.java
+++ Main.java
@@ -1,3 +1,9 @@
+/*This program uses recursion with stacks data structure and solves a binary maze and outputs the path it takes as well as 
+ *whether or not the maze was solvable to being with. It reads the dimensions of a maze, 
+ *the maze matrix and the starting and ending points from a text file. 
+ */
+
+//This code is owned by Pegah Eizadkhah and is not meant for redistribution. 
 //Main (driver) class for the maze-solving program
 
 import java.util.*;
