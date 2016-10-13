69fbeaaae30e1a2ad5cfa8e2d8f4187927ec39e7
Pegah-Eizadkhah <peizadkh@gmail.com>
2016-10-12 21:47:43 -0400

    Created Main.java

---
+++ Main.java
@@ -0,0 +1,34 @@
+//Main (driver) class for the maze-solving program
+
+import java.util.*;
+public class Main
+{
+    public static void main (String [] args)
+    {   
+    	//create new scanner object
+        Scanner keyboard = new Scanner(System.in);
+        //create object of MazeGame class
+        MazeRecursive  mmgg = new MazeRecursive();
+        //read name of file
+        System.out.println("Please enter the name of the maze file: ");
+        String mazeFile = keyboard.next();
+
+        //call on MazeGame method
+        //System.out.println(mmgg.rows + " " + mmgg.cols);
+        mmgg.createMaze(mazeFile);
+        //System.out.println(mmgg.rows + " " + mmgg.cols);
+        mmgg.initializeVisitedArray();
+	     
+        //print results
+        if(mmgg.isPath())
+        {
+        	System.out.println();
+            System.out.println("Maze was solved!");
+        }
+        else
+        {
+        	System.out.println();
+            System.out.println("Maze could not be solved.");
+        }       
+    }
+}
